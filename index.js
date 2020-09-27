4896b199aacfd2d493d0510d90b862f513985f51
Hajar Helal <hajerhelal44@gmail.com>
2020-09-27 00:18:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Your code here
-function mapToNegativize(sourceArray){
-
+function mapToNegativize(sourceArray) {
+  let r = []
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    r.push(-1 * sourceArray[i])
+  }
+  return r
 }
