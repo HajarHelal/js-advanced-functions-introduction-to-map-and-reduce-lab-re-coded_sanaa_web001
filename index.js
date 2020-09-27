a925fca921670409659404e8e18a9e810475d550
Hajar Helal <hajerhelal44@gmail.com>
2020-09-27 00:27:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,10 +32,26 @@ function mapToSquare(sourceArray) {
 }
 ////////////////////
 
+function reduceToTotal(sourceArray, startingPoint=0) {
+  let total = startingPoint
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    total = total + sourceArray[i]
+  }
+  return total
+}
+
 function reduceToAllTrue(sourceArray) {
   for (let i = 0; i < sourceArray.length; i++ ) {
     if (!sourceArray[i])
      return false
   }
   return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    if (sourceArray[i])
+     return true
+  }
+  return false
 }
